db843129a44f11c7992ad54386199c1da78c9a3c
Sanfelix <sanfelix@yandex.ru>
2023-06-09 20:25:19 +0900

    add seminar1_2-code

--- Seminar1_2/Program.cs
+++ Seminar1_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+if(numb1 == numb2 * numb2)
+{
+    Console.WriteLine("Первое чило является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое чило не является квадратом второго"); 
+}
