12c4a57029d3db14ca6fd9a016a4ecd395ed630f
Sanfelix <sanfelix@yandex.ru>
2023-06-09 19:39:47 +0900

    add seminar1+code

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine("Today is Friday");
+Console.WriteLine("Today is Friday");
+Console.ReadLine();
+*/
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введено значение " + number);
+Console.WriteLine($"Введен номер {number}");
