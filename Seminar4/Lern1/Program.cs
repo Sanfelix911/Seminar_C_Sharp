e055b94fa4a94f714fb61983a9c41230e622dcb5
Sanfelix <sanfelix@yandex.ru>
2023-06-20 19:19:09 +0900

    add Lr

---
+++ Seminar4/Lern1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Принимает число А и выдает сумму от 1 до А
+
+void Sum1ToA(int a)
+{
+    int sum = 0;
+    for (int i = 0; i <= a; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine(sum);
+}
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sum1ToA(10);
