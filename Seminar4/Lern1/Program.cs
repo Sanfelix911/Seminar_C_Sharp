9448cdc6d70d29e3ad6e7a6ed693514c181dadfe
Sanfelix <sanfelix@yandex.ru>
2023-06-20 20:27:51 +0900

    add all

--- Seminar4/Lern1/Program.cs
+++ Seminar4/Lern1/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Принимает число А и выдает сумму от 1 до А
 
-void Sum1ToA(int a)
+/*void Sum1ToA(int a)
 {
     int sum = 0;
     for (int i = 0; i <= a; i++)
@@ -11,4 +11,28 @@ void Sum1ToA(int a)
 }
 Console.Write("Введите число ");
 int n = Convert.ToInt32(Console.ReadLine());
-Sum1ToA(10);
+Sum1ToA(10);*/
+//Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+int[] NewArray(int size_array)
+{
+    int[] random1Arrai = new int[size_array];
+    for (int i = 0; i < size_array; i++)
+    {
+        random1Arrai[i] = new Random().Next(0, 2);
+    }
+    return random1Arrai;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("Ваш массив");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+int size=8;
+int[] array1 = NewArray(size);
+PrintArray(array1);
