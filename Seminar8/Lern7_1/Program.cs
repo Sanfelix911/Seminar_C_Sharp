af1acf05925d9c2054168f104c92290e1ddb4953
Sanfelix <sanfelix@yandex.ru>
2023-07-08 11:10:05 +0900

    add all seminar

---
+++ Seminar8/Lern7_1/Program.cs
@@ -0,0 +1,90 @@
+﻿/*Задача 1.
+//Задайте двумерный массив. Напишите программу, которая поменяет местами две любые строки массива.
+
+int [,] Created2dArray (int rows, int columns, int minV, int maxV)
+{
+    int [,] createdArray = new int [rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            createdArray[i,j] = new Random().Next(minV, maxV+1);
+    return createdArray;
+}
+
+void ShowArray (int [,] printedArray)
+{
+    for (int i = 0; i < printedArray.GetLength(0); i++)
+    {    
+        for (int j = 0; j < printedArray.GetLength(1); j++)
+        {
+            Console.Write(printedArray[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();    
+}
+
+int [,] ChangeRows (int [,] arrayToChange, int row1, int row2)
+{
+    if (row1 >= 0 && row1 < arrayToChange.GetLength(0) && 
+        row1 != row2 &&
+        row2 >= 0 && 
+        row2 < arrayToChange.GetLength(0))
+        {
+           for (int j = 0; j < arrayToChange.GetLength(1); j++)
+           {
+                int temp = arrayToChange[row1, j];
+                arrayToChange[row1, j] = arrayToChange[row2, j];
+                arrayToChange[row2, j] = temp;
+           } 
+        }
+    else Console.WriteLine("Error! Impossible value!");
+    return arrayToChange;
+}
+
+Console.Write("Input count of rows: ");
+int userRows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input count of columns: ");
+int userColumns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min value: ");
+int userMin = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max value: ");
+int userMax = Convert.ToInt32(Console.ReadLine());
+
+int [,] newArray = Created2dArray(userRows, userColumns, userMin, userMax);
+ShowArray(newArray);
+
+Console.Write($"Input number of the first row to remove from 0 to {userRows-1}: ");
+int userFirstRow = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Input number of the second row to remove from 0 to {userRows-1}: ");
+int userSecondRow = Convert.ToInt32(Console.ReadLine());
+
+ShowArray(ChangeRows(newArray, userFirstRow, userSecondRow));*/
+
+//Задача 2.
+// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+//Задача 3.
+// Задайте двумерный массив из целых чисел. Напишите программу, которая обнулит строку и столбец, 
+// на пересечении которых расположен первый наименьший элемент массива.
+
+/*int[,] ExchRowsCols (int[,] arrToChange)
+{
+    int[,] tempArr = new int[arrToChange.GetLength(0), arrToChange.GetLength(1)];
+    if (arrToChange.GetLength(0) == arrToChange.GetLength(1))
+    {
+        for (int i = 0; i < arrToChange.GetLength(0); i++)
+        {
+            for (int j = 0; j < arrToChange.GetLength(1); j++)
+            {
+                tempArr[j, i] = arrToChange[i, j];
+            }
+        }
+        return tempArr;
+    }
+    else
+    {
+        Console.WriteLine("Impossible to exchange rows with columns. Array is not square.");
+        return arrToChange;
+    }
+}*/
---
+++ Seminar9/Lern9_1/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+void ShovNumb(int n){
+    if(n>1) ShovNumb(n-1);
+    Console.Write(n+" ");
+}
+ShovNumb(5);*/
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 8; N = 4 -> "4, 6, 7, 8"
+
+// void ShowNumRange(int n, int m){
+//     if (n != m) ShowNumRange(Math.Min(n,m), Math.Max(n,m)-1);
+//     Console.Write($"{Math.Max(n,m)} ");
+// }
+
+// ShowNumRange(10,20);
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// 453 -> 12
+// -45 -> 9
+
+// int SumDigits(int n){
+//     if (n<0) n *= (-1);
+//     if (n==0) return 0;
+//     return SumDigits(n/10) + n % 10;     
+// }
+
+// Console.WriteLine("Enter num:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int summ = SumDigits(Convert.ToInt32(Console.ReadLine()));
+// Console.Write($"Your sum is {summ}");
+
+//Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+//A = 3; B = 5 -> 243 (3⁵)
+//A = 2; B = 3 -> 8
+
+// 2^(-1) = 1/2 = 0.5
+// 2^(-3) = 1/(2^3) = 1/8 = 0.125
+// -2^0 = 1
+// 1/2*2*2 = 1/8 = 0,125
+
+// double FindPow(int a, int b){
+//     if(b == 0) return 1.0;
+//     if(b > 0) return FindPow(a,b-1) * a;
+//     return FindPow(a, b+1) / a;
+// }
+
+// double res = FindPow(5, 4);
+// Console.WriteLine(res);
+
+
